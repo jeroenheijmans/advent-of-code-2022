74a17dc34f6e35d6c09f0cb3a914ec8c5b9ac1d9
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-12-01 09:12:44 +0100

    Infi puzzle part 2 (partial)

--- src/aoc.infi.nl/2022.js
+++ src/aoc.infi.nl/2022.js
@@ -189,16 +189,46 @@ const rotations = {
     "-360": -8,
 };
 
+let grid = {}, maxx = 0, maxy = 0, minx = 0, miny = 0;
+
 for (let i = 0; i < data.length; i++) {
     if (data[i][0] === "draai") {
         vectorIndex = (vectorIndex + vectors.length + rotations[data[i][1]]) % vectors.length;
     } else {
         const step = parseInt(data[i][1]);
-        pos[0] = pos[0] + step * vectors[vectorIndex][0];
-        pos[1] = pos[1] + step * vectors[vectorIndex][1];
+
+        for (let x = step; x > 0; x--) {
+            grid[pos[1]] = grid[pos[1]] || {};
+            grid[pos[1]][pos[0]] = "#";
+            
+            pos[0] = pos[0] + vectors[vectorIndex][0];
+            pos[1] = pos[1] + vectors[vectorIndex][1];
+
+            maxx = Math.max(maxx, pos[0]);
+            minx = Math.min(minx, pos[0]);
+            maxy = Math.max(maxy, pos[1]);
+            miny = Math.min(miny, pos[1]);
+        }
+        
+        grid[pos[1]] = grid[pos[1]] || {};
+        grid[pos[1]][pos[0]] = "#";
     }
 }
 
 let part1 = Math.abs(pos[0]) + Math.abs(pos[1]);
 
 console.log("Part 1 ended at", pos, "solution is then:", part1);
+
+console.log("Part 2:");
+let part2 = "";
+for (let y = miny - 2; y < maxy + 3; y++) {
+    for (let x = minx - 2; x < maxx + 3; x++) {
+        if (grid[y] && grid[y][x]) {
+            part2 += " ";
+        } else {
+            part2 += "#";
+        }
+    }
+    part2 += "\n";
+}
+console.log(part2);
