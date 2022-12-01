a3d210ecd8183833b9e06422ae7208a68a7c217d
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-12-01 08:35:22 +0100

    Infi puzzle part 1

---
+++ src/aoc.infi.nl/2022.js
@@ -0,0 +1,204 @@
+const input = `
+draai 90
+spring 0
+draai -90
+loop 3
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 90
+loop 4
+draai -90
+draai 90
+spring 47
+draai 90
+spring 6
+draai 180
+loop 5
+draai 45
+loop 1
+draai 45
+loop 2
+draai 45
+loop 1
+draai 45
+loop 2
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 180
+draai 90
+spring -37
+draai -90
+draai -90
+loop 4
+draai 90
+loop 3
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 90
+loop 4
+draai -90
+draai 90
+spring 12
+draai 90
+spring 6
+draai 180
+loop 6
+draai -90
+loop 2
+draai 180
+loop 4
+draai -90
+draai 90
+spring -21
+draai 90
+spring 6
+draai 180
+draai -90
+loop 4
+draai 90
+loop 3
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 90
+loop 4
+draai -90
+draai 90
+spring 56
+draai 90
+spring 6
+draai 180
+draai 90
+loop 2
+draai -45
+loop 2
+draai -45
+loop 2
+draai 315
+loop 2
+draai -45
+loop 2
+draai -90
+loop 6
+draai 180
+draai 90
+spring -32
+draai -90
+loop 6
+draai 135
+loop 3
+draai -90
+loop 3
+draai 135
+loop 6
+draai 180
+draai 90
+spring 3
+draai -90
+loop 5
+draai 45
+loop 1
+draai 45
+loop 2
+draai 45
+loop 1
+draai 45
+loop 2
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 180
+draai 90
+spring 10
+draai -90
+loop 6
+draai 135
+loop 6
+draai -135
+loop 6
+draai 90
+spring -43
+draai 90
+spring 6
+draai 180
+draai 90
+loop 3
+draai -45
+loop 1
+draai -45
+loop 1
+draai -45
+loop 1
+draai -45
+loop 2
+draai 45
+loop 1
+draai 45
+loop 1
+draai 45
+loop 1
+draai 45
+loop 3
+draai -90
+`;
+
+const data = input
+    .trim()
+    .split(/\r?\n/)
+    .filter(x => !!x)
+    .map(x => x.split(" "))
+    ;
+
+const pos = [0, 0];
+const vectors = [[0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1, 1]];
+let vectorIndex = 0;
+const rotations = {
+    "45": 1,
+    "90": 2,
+    "135": 3,
+    "180": 4,
+    "225": 5,
+    "270": 6,
+    "315": 7,
+    "360": 8,
+    "-45": -1,
+    "-90": -2,
+    "-135": -3,
+    "-180": -4,
+    "-225": -5,
+    "-270": -6,
+    "-315": -7,
+    "-360": -8,
+};
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i][0] === "draai") {
+        vectorIndex = (vectorIndex + vectors.length + rotations[data[i][1]]) % vectors.length;
+    } else {
+        const step = parseInt(data[i][1]);
+        pos[0] = pos[0] + step * vectors[vectorIndex][0];
+        pos[1] = pos[1] + step * vectors[vectorIndex][1];
+    }
+}
+
+let part1 = Math.abs(pos[0]) + Math.abs(pos[1]);
+
+console.log("Part 1 ended at", pos, "solution is then:", part1);
