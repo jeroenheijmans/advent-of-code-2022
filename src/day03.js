c6cdd9d384e543fb9f2dd8e57387bcb2136fca47
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-12-02 22:56:12 +0100

    Day 3 preparation

---
+++ src/day03.js
@@ -0,0 +1,19 @@
+const input = `
+`;
+
+const data = input
+  .trim()
+  .split(/\r?\n/)
+  .filter(x => !!x)
+  .map(x => x)
+  ;
+
+let part1 = 0;
+let part2 = 0;
+
+for (let i = 0; i < data.length; i++) {
+
+}
+
+console.log("Part 1:", part1);
+console.log("Part 2:", part2);
