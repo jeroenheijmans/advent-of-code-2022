641340f577406b8182ad96996b178c179394bb7d
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-12-05 08:00:12 +0100

    Day 05 part 1 (start with input parsing)

--- src/day05.js
+++ src/day05.js
@@ -1,20 +1,49 @@
 const input = `
+[D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
 `;
 
-const data = input
+function parseArrangements(text) {
+  text = text.replaceAll("    ", " [ ]");
+  text = text.replaceAll("]", "");
+  text = text.replaceAll("[", ";");
+  text = text.replaceAll(" ", "");
+
+  const lines = text.split(/\r?\n/).map(x => x.split(";"));
+  const stacks = [];
+  for (let y = 0; y < lines.length - 1; y++) {
+    lines[y].forEach((val, idx) => {
+      stacks[idx] = stacks[idx] || [];
+      if (val) stacks[idx].unshift(val);
+    });
+  }
+  return stacks;
+}
+
+const [text1, text2] = input
   .trim()
-  .split(/\r?\n/)
-  .filter(x => !!x)
-  .map(x => x)
+  .split(/\r?\n\r?\n/)
   ;
+
+const arrangements = parseArrangements(text1);
+const instructions = text2
+  .replaceAll(/(move )|(from )|(to )/g, "")
+  .split(/\r?\n/)
+  .map(x => x.split(" "))
+  .map(x => ({ source: x[0], from: x[1], to: x[2] }));
+
+console.log(arrangements);
+console.log(instructions);
 
 let part1 = 0;
 let part2 = 0;
-
-data.forEach(row => {
-  part1++;
-});
 
 console.log("Part 1:", part1);
 console.log("Part 2:", part2);
