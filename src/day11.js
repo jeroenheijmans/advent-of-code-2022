8055ba8b8f22630e54e9315f9c2a2d077a6678f7
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-12-11 06:41:00 +0100

    Day 11 part 2 (incorrect)

--- src/day11.js
+++ src/day11.js
@@ -1,59 +1,32 @@
 const input = `
 Monkey 0:
-  Starting items: 83, 88, 96, 79, 86, 88, 70
-  Operation: new = old * 5
-  Test: divisible by 11
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
     If true: throw to monkey 2
     If false: throw to monkey 3
 
 Monkey 1:
-  Starting items: 59, 63, 98, 85, 68, 72
-  Operation: new = old * 11
-  Test: divisible by 5
-    If true: throw to monkey 4
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
     If false: throw to monkey 0
 
 Monkey 2:
-  Starting items: 90, 79, 97, 52, 90, 94, 71, 70
-  Operation: new = old + 2
-  Test: divisible by 19
-    If true: throw to monkey 5
-    If false: throw to monkey 6
-
-Monkey 3:
-  Starting items: 97, 55, 62
-  Operation: new = old + 5
-  Test: divisible by 13
-    If true: throw to monkey 2
-    If false: throw to monkey 6
-
-Monkey 4:
-  Starting items: 74, 54, 94, 76
+  Starting items: 79, 60, 97
   Operation: new = old * old
-  Test: divisible by 7
-    If true: throw to monkey 0
+  Test: divisible by 13
+    If true: throw to monkey 1
     If false: throw to monkey 3
 
-Monkey 5:
-  Starting items: 58
-  Operation: new = old + 4
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
   Test: divisible by 17
-    If true: throw to monkey 7
+    If true: throw to monkey 0
     If false: throw to monkey 1
 
-Monkey 6:
-  Starting items: 66, 63
-  Operation: new = old + 6
-  Test: divisible by 2
-    If true: throw to monkey 7
-    If false: throw to monkey 5
-
-Monkey 7:
-  Starting items: 56, 56, 90, 96, 68
-  Operation: new = old + 7
-  Test: divisible by 3
-    If true: throw to monkey 4
-    If false: throw to monkey 1
 `;
 
 const data = input
@@ -72,6 +45,7 @@ data.forEach(line => {
     current = {
       nr: number++,
       items: [],
+      divisor: null,
       operation: null,
       iftrue: null,
       iffalse: null,
@@ -86,6 +60,7 @@ data.forEach(line => {
   if (line.includes("Test: divisible by ")) {
     const nr = parseInt(line.replace("Test: divisible by ", ""));
     current.test = (lvl) => lvl % nr === 0;
+    current.divisor = nr;
   }
   if (line.includes("Operation: new = ")) {
     const stuff = line.replace("Operation: new = old ", "");
@@ -113,9 +88,36 @@ data.forEach(line => {
   }
 });
 
+let bigdivisor = monkeys.map(m => m.divisor).reduce((curr,prev) => curr * prev, 1);
+
 for (let round = 0; round < 20; round++) {
   monkeys.forEach(monkey => {
     // console.log("Monkey", monkey.nr, " items ", monkey.items);
+
+    for (let i = 0; i < monkey.items.length; i++) {
+      monkey.inspections++;
+      
+      monkey.items[i] = monkey.operation(monkey.items[i]);
+      monkey.items[i] = Math.trunc(monkey.items[i] / bigdivisor);
+
+      // console.log("    changes were made, now:", monkey.items);
+      if (isNaN(monkey.items[i])) throw new Error("Here");
+      
+      if (monkey.test(monkey.items[i])) {
+        monkeys[monkey.iftrue].items.push(monkey.items[i]);
+      } else {
+        monkeys[monkey.iffalse].items.push(monkey.items[i]);
+      }
+
+    }
+    monkey.items = [];
+  });
+}
+
+for (let round = 0; round < 10000; round++) {
+  monkeys.forEach(monkey => {
+    // console.log("Monkey", monkey.nr, " items ", monkey.items);
+    if (round % 100 === 0) console.log(round);
 
     for (let i = 0; i < monkey.items.length; i++) {
       monkey.inspections++;
