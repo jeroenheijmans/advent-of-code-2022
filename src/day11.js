eb404cbc85a48f8d81663c30e66e661947738f1e
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-01-01 20:28:13 +0100

    Day 11 part 2

--- src/day11.js
+++ src/day11.js
@@ -1,30 +1,58 @@
 const input = `
 Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
+  Starting items: 83, 88, 96, 79, 86, 88, 70
+  Operation: new = old * 5
+  Test: divisible by 11
     If true: throw to monkey 2
     If false: throw to monkey 3
 
 Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
+  Starting items: 59, 63, 98, 85, 68, 72
+  Operation: new = old * 11
+  Test: divisible by 5
+    If true: throw to monkey 4
     If false: throw to monkey 0
 
 Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
+  Starting items: 90, 79, 97, 52, 90, 94, 71, 70
+  Operation: new = old + 2
+  Test: divisible by 19
+    If true: throw to monkey 5
+    If false: throw to monkey 6
 
 Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
+  Starting items: 97, 55, 62
+  Operation: new = old + 5
+  Test: divisible by 13
+    If true: throw to monkey 2
+    If false: throw to monkey 6
+
+Monkey 4:
+  Starting items: 74, 54, 94, 76
+  Operation: new = old * old
+  Test: divisible by 7
     If true: throw to monkey 0
+    If false: throw to monkey 3
+
+Monkey 5:
+  Starting items: 58
+  Operation: new = old + 4
+  Test: divisible by 17
+    If true: throw to monkey 7
+    If false: throw to monkey 1
+
+Monkey 6:
+  Starting items: 66, 63
+  Operation: new = old + 6
+  Test: divisible by 2
+    If true: throw to monkey 7
+    If false: throw to monkey 5
+
+Monkey 7:
+  Starting items: 56, 56, 90, 96, 68
+  Operation: new = old + 7
+  Test: divisible by 3
+    If true: throw to monkey 4
     If false: throw to monkey 1
 `;
 
@@ -88,54 +116,30 @@ data.forEach(line => {
   }
 });
 
-const statemap = {};
-let givenstate = null;
+const denominator = monkeys.map(m => m.divisor).reduce((acc, curr) => acc * curr, 1);
 
 for (let round = 0; round < 10000; round++) {
-  if (round === 1 || round === 20 || round === 1000) {
-    console.log("------------------------------------------------------", round);
-    monkeys.forEach(m => console.log("Monkey", m.nr, "inspected", m.inspections, "times."));
-  }
-
-  const startState = givenstate || monkeys.map(m => JSON.stringify({ nr: m.nr, items: m.items.map(i => i.itemid) }));
-
-  if (statemap.hasOwnProperty(startState) && statemap.hasOwnProperty(statemap[startState].newState)) {
-    Object.keys(statemap[startState].inspectsPerMonkey)
-      .forEach(key => monkeys[key].inspections += statemap[startState].inspectsPerMonkey[key]);
-    givenstate = statemap[startState].newState;
-    continue;
-  }
-
-  const inspectsPerMonkey = {};
-  
   monkeys.forEach(monkey => {
-    inspectsPerMonkey[monkey.nr] = 0;
     for (let i = 0; i < monkey.items.length; i++) {
-      inspectsPerMonkey[monkey.nr]++;
+      monkey.inspections++;
       monkey.items[i].worry = monkey.operation(monkey.items[i].worry);
+      monkey.items[i].worry = monkey.items[i].worry % denominator;
 
       if (monkey.test(monkey.items[i].worry)) {
         monkeys[monkey.iftrue].items.push(monkey.items[i]);
       } else {
         monkeys[monkey.iffalse].items.push(monkey.items[i]);
       }
+
     }
     monkey.items = [];
-    monkey.inspections += inspectsPerMonkey[monkey.nr];
   });
-
-  const endState = monkeys.map(m => JSON.stringify({ nr: m.nr, items: m.items.map(i => i.itemid) }));
-
-  statemap[startState] = {
-    inspectsPerMonkey,
-    newState: endState,
-  };
 }
 
 let inspections1 = monkeys.map(m => m.inspections).sort((a,b) => b-a);
 let part1 = inspections1[0] * inspections1[1];
 let part2 = 0;
-console.log(inspections1);
+
 console.log("Part 1", part1);
 console.log("Part 2", part2);
 
